4560ef27feeb90cd52a46561c314a5fda742c20d
xnki7 <codeinja7@gmail.com>
2024-12-31 08:47:47 +0530

    Initialised project backend

---
+++ second-brain-backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+const app = express();
+
+app.post("/api/v1/signup", async (req, res) => {
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+});
+
+app.post("/api/v1/content", async (req, res) => {
+});
+
+app.get("/api/v1/content", async (req, res) => {
+});
+
+app.delete("/api/v1/content", async (req, res) => {
+});
+
+app.post("/api/v1/brain/share", async (req, res) => {
+});
+
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+});
